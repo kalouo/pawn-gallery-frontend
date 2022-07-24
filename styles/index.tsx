873c3f92c7672e0b8aa2875f5d2d47dc11100554
kalouo <23177052+kalouo@users.noreply.github.com>
2022-07-24 13:56:37 +0200

    test: husky

--- styles/index.tsx
+++ styles/index.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider as StyledComponentsThemeProvider } from "styled-component
 
 import {theme} from "./theme"
 
+
+
+
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
     return (
       <StyledComponentsThemeProvider theme={theme}>
