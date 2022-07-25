a43e317c2dabe7376d62a81417a6ff527a6121dd
kalouo <23177052+kalouo@users.noreply.github.com>
2022-07-25 10:30:03 +0200

    fix: shortenAddress

--- utils/address.ts
+++ utils/address.ts
@@ -1,3 +1,5 @@
+const ADDRESS_LENGTH = 36;
+
 export function shortenAddress(address: string, chars = 4): string {
-  return `${address.substring(0, chars + 2)}...${address.substring(42 - chars)}`;
+  return `${address.substring(0, chars + 2)}...${address.substring(ADDRESS_LENGTH - chars)}`;
 }
