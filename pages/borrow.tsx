796d721f86b71fc02137ecb5659e2de5e9c89a1e
kalouo <23177052+kalouo@users.noreply.github.com>
2022-07-26 19:13:28 +0200

    feat: borrow-lend routes

---
+++ pages/borrow.tsx
@@ -0,0 +1,12 @@
+import { View } from 'components';
+import type { NextPage } from 'next';
+
+const Borrow: NextPage = () => {
+  return (
+    <div>
+      <View>Borrow</View>
+    </div>
+  );
+};
+
+export default Borrow;
---
+++ pages/lend.tsx
@@ -0,0 +1,12 @@
+import { View } from 'components';
+import type { NextPage } from 'next';
+
+const Lend: NextPage = () => {
+  return (
+    <div>
+      <View>Lend</View>
+    </div>
+  );
+};
+
+export default Lend;
