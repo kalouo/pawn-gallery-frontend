b7ae47438b6030894612e42789b8af1aff1da37e
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-04 20:54:52 +0200

    refactor: user assets page

--- pages/borrow.tsx
+++ pages/borrow.tsx
@@ -1,19 +1,7 @@
 import type { NextPage } from 'next';
-import useSWR from 'swr';
-
-import { requestor } from 'graphql/teztok';
-import { useWallet } from 'hooks/useWallet';
 
 import { queryUserAssets } from 'graphql/teztok/queries';
-import AssetCard from 'components/AssetCard';
-import styled from 'styled-components';
-
-const BorrowContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  padding: 100px 120px;
-`;
+import { useWallet } from 'hooks/useWallet';
 
 const Borrow: NextPage = () => {
   const { address, initialized } = useWallet();
@@ -21,18 +9,41 @@ const Borrow: NextPage = () => {
   const { data, error } = queryUserAssets({ address });
 
   return (
-    <BorrowContainer>
-      {data?.holdings?.map((asset: { [key: string]: any }, index: number) => (
-        <AssetCard
-          key={`user_asset_${index}`}
-          thumbnailUri={asset?.token?.thumbnail_uri}
-          name={asset?.token?.name}
-          tokenId={asset?.token?.token_id}
-          tokenAddress={asset?.token?.fa2_address}
-          platform={asset?.token?.platform}
-        />
-      ))}
-    </BorrowContainer>
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Your Collection</h2>
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data?.holdings?.map((asset: { [key: string]: any }, index: number) => {
+            const imageSrc = asset?.token?.thumbnail_uri?.replace(
+              'ipfs://',
+              'https://ipfs.io/ipfs/'
+            );
+
+            return (
+              <div key={`holding-${index}`} className="group relative">
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                  <img
+                    src={imageSrc}
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href={`borrow/${asset?.token?.fa2_address}/${asset?.token?.token_id}`}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {asset?.token?.name}
+                      </a>
+                    </h3>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{asset?.token?.platform}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
 
