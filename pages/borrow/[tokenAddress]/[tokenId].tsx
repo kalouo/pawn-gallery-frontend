6d996e3c8a687dc8326cf9bb59878779bdb365d8
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-04 21:03:06 +0200

    wip tokenId

--- pages/borrow/[tokenAddress]/[tokenId].tsx
+++ pages/borrow/[tokenAddress]/[tokenId].tsx
@@ -1,4 +1,3 @@
-import { ExclamationCircleIcon } from '@heroicons/react/solid';
 import { joiResolver } from '@hookform/resolvers/joi';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/router';
@@ -9,18 +8,24 @@ import type { NextPage } from 'next';
 
 import { queryToken } from 'graphql/teztok/queries';
 import InputField from 'components/InputField';
-
+import Selection from 'components/Selection';
 interface IFormInputs {
   loanAmount: string;
 }
 
 const schema = Joi.object({
   loanAmount: Joi.number().required(),
+  loanInterest: Joi.number().required(),
+  loanCurrency: Joi.string().required(),
 });
 
-const currencies = [{ name: 'USDT' }, { name: 'EURL' }, { name: 'XTZ' }];
+const currencies = [
+  { name: 'XTZ', imageUrl: '/images/currencies/xtz.png' },
+  { name: 'USDT', imageUrl: '/images/currencies/usdt.png' },
+  { name: 'EURL', imageUrl: '/images/currencies/eurl.png' },
+];
 
-const BorrowForm: NextPage = () => {
+export default function Borrow() {
   const {
     register,
     formState: { errors: formErrors },
@@ -33,51 +38,104 @@ const BorrowForm: NextPage = () => {
 
   const router = useRouter();
 
-  const [selectedCurrency, setSelectedCurrency] = useState(currencies[0]);
-
   const { tokenAddress, tokenId } = router.query;
   const { data, error } = queryToken({ tokenAddress, tokenId });
   const token = data?.tokens[0];
 
   const src = token?.thumbnail_uri?.replace('ipfs://', 'https://ipfs.io/ipfs/');
 
-  return (
-    <div className="p-12">
-      <div className="md:grid md:grid-cols-3 md:gap-6">
-        <div className="md:col-span-1">
-          <div className="px-4 sm:px-0">
-            {<Image layout="responsive" width={200} height={200} src={src} />}
-          </div>
-        </div>
-        <div className="mt-5 md:mt-0 md:col-span-2">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="shadow overflow-hidden sm:rounded-md">
-              <div className="px-4 py-5 bg-black sm:p-6">
-                <div className="flex justify-between	">
-                  <div> Loan Amount </div>
-                  <InputField
-                    register={register}
-                    rules={{ required: true }}
-                    type="number"
-                    name="loanAmount"
-                    errors={formErrors}
-                  ></InputField>
-                </div>
-              </div>
-              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+  const BorrowForm = () => {
+    return (
+      <div className="max-w-sm lg:w-96 p-4">
+        <div className="mt-6 text-2xl text-gray-800">List Collateral</div>
+
+        <div className="mt-8">
+          <div className="mt-6">
+            <form onSubmit={handleSubmit(onSubmit)} action="#" method="POST" className="space-y-6">
+              <Selection
+                register={register}
+                name="loanCurrency"
+                rules={{ required: true }}
+                label="Loan Currency"
+                list={currencies.map((item, index) => ({ ...item, id: `ccy-${index}` }))}
+              ></Selection>
+
+              <InputField
+                register={register}
+                type="number"
+                name="loanAmount"
+                errors={formErrors}
+                label="Loan Amount"
+              ></InputField>
+
+              <InputField
+                register={register}
+                type="number"
+                name="loanInterest"
+                errors={formErrors}
+                label="Loan Interest"
+              ></InputField>
+
+              <InputField
+                register={register}
+                type="number"
+                name="loanInterest"
+                errors={formErrors}
+                label="Expiry"
+              ></InputField>
+
+              <div>
                 <button
                   type="submit"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Save
+                  Submit
                 </button>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default BorrowForm;
+  const PawnImage = () => {
+    return (
+      <div className="max-w-sm lg:w-96 p-4">
+        <div className="mt-6 text-3xl text-black">Request A Loan</div>
+        <div className="mt-8">
+          <div className="mt-6">
+            <div className="max-w-sm lg:w-96 grid">
+              <img src={src} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const PawnInformation = () => {
+    return (
+      <div className="max-w-sm lg:w-96 p-4">
+        <div className="mt-6 text-2xl text-gray-800">NFT Details</div>
+        <div className="mt-8">
+          <div>
+            <div>Platform: {token?.platform}</div>
+            <div>Name: {token?.name}</div>
+            <div>Creator: {token?.creator}</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="flex justify-center sm:justify-start py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24 flex-wrap sm:flex-nowrap">
+        <PawnImage />
+        <PawnInformation />
+        <BorrowForm />
+      </div>
+    </>
+  );
+}
