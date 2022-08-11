5ef42bba9be498a8d4373c51c486b55e41769a03
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-11 21:05:30 +0200

    WIP: setup script

---
+++ contract-types.ts
@@ -0,0 +1,3 @@
+export type { ContractsLoanCoreStep000Cont0ContractContractType as LoanCore } from 'types/contracts/loan_core/step_000_cont_0_contract.types';
+export type { ContractsLoanNoteStep000Cont0ContractContractType as LoanNote } from 'types/contracts/loan_note/step_000_cont_0_contract.types';
+export type { ContractsCollateralVaultStep000Cont0ContractContractType as CollateralVault } from 'types/contracts/collateral_vault/step_000_cont_0_contract.types';
