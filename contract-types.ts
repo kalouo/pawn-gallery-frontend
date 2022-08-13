35822a6586b535f57d860c56e6db307021d6a272
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-13 19:11:43 +0200

    add types (2)

--- contract-types.ts
+++ contract-types.ts
@@ -1,3 +1,6 @@
 export type { ContractsLoanCoreStep000Cont0ContractContractType as LoanCore } from 'types/contracts/loan_core/step_000_cont_0_contract.types';
 export type { ContractsLoanNoteStep000Cont0ContractContractType as LoanNote } from 'types/contracts/loan_note/step_000_cont_0_contract.types';
 export type { ContractsCollateralVaultStep000Cont0ContractContractType as CollateralVault } from 'types/contracts/collateral_vault/step_000_cont_0_contract.types';
+
+export type { ContractsTestCurrencyStep000Cont0ContractContractType as FA2Fungible } from 'types/contracts/test_currency/step_000_cont_0_contract.types';
+export type { ContractsTestNftStep000Cont0ContractContractType as FA2NFT } from 'types/contracts/test_nft/step_000_cont_0_contract.types';
