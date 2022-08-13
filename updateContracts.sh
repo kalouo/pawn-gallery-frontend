0906d745b9b9318a18d1af554f421ab30f2f3e56
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-13 09:30:35 +0200

    refactor updateContracts.sh

--- updateContracts.sh
+++ updateContracts.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
 # Pull latest contracts from remote repository.
-git submodule update --remote
+echo -e "\xE2\x9C\x94"
+echo "Updating remote submodule ..."
+$(git submodule update --remote)
+
+echo "Compiling contracts ..."
+cd contracts && chinstrap compile
 
 # Generate Typescript bindings.
-taq generate types
+echo "Generating types ..."
+cd .. && taq generate types > /dev/null
+echo "✔ Types generated."
+
