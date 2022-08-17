484bf47e20674855da21f1c949400287c1850e28
kalouo <23177052+kalouo@users.noreply.github.com>
2022-08-17 10:50:50 +0200

    add type

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -101,7 +101,7 @@ export default function Example() {
                       >
                         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <Menu.Item>
-                            {({ active }) => (
+                            {({ active }: { active: boolean }) => (
                               <a
                                 onClick={disconnect}
                                 className={classNames(
