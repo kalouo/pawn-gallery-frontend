54390abfc4bbff90c8602216cb7dd0d059871420
kalouo <23177052+kalouo@users.noreply.github.com>
2022-09-07 13:49:46 +0200

    feat: tabs

---
+++ components/Tabs.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+interface TabsProps {
+  tabItems: {
+    name: string;
+    content: React.ReactNode;
+  }[];
+}
+
+const Tabs = ({ tabItems }: TabsProps) => {
+  const [openTab, setOpenTab] = useState(0);
+  const color = 'indigo';
+
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <ul
+            className="flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4"
+            role="tablist"
+          >
+            {tabItems.map((item, index) => (
+              <li className="flex-auto text-center">
+                <a
+                  className={
+                    `uppercase px-6 py-3 block sm:border-2 sm:border-${color}-600 ` +
+                    (openTab === index
+                      ? 'text-white bg-' + color + '-600'
+                      : 'text-' + color + '-600 bg-white ') +
+                    (index === 0 ? '' : ' sm:border-l-0')
+                  }
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTab(index);
+                  }}
+                  data-toggle="tab"
+                  href="#link1"
+                  role="tablist"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="">
+            <div className="tab-content tab-space">
+              {tabItems.map((item, index) => (
+                <div
+                  className={openTab === index ? 'block' : 'hidden'}
+                  id={`borrow-content-${index}`}
+                >
+                  {item.content}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tabs;
--- pages/borrow.tsx
+++ pages/borrow.tsx
@@ -4,48 +4,65 @@ import type { NextPage } from 'next';
 import PleaseConnect from 'components/PleaseConnect';
 import { useWeb3 } from 'hooks/useWeb3';
 import { useHoldings } from 'hooks/useHoldings';
+import Tabs from 'components/Tabs';
+
+const Holdings = ({ address }: { address: string }) => {
+  const { data: holdings } = useHoldings(address);
+
+  return (
+    <div className="max-w-2xl mx-auto py-4 px-4 sm:px-0 sm:py-12 lg:max-w-7xl">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {holdings?.map((asset: { [key: string]: any }, index: number) => {
+          const imageSrc = asset?.token?.thumbnail_uri?.replace('ipfs://', 'https://ipfs.io/ipfs/');
+
+          return (
+            <div key={`holding-${index}`} className="group relative">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <img
+                  src={imageSrc}
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <Link href={`borrow/${asset?.token?.fa2_address}/${asset?.token?.token_id}`}>
+                      <a>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {asset?.token?.name}
+                      </a>
+                    </Link>
+                  </h3>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{asset?.token?.platform}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 const Borrow: NextPage = () => {
   const { address } = useWeb3();
 
-  const { data: holdings } = useHoldings(address as string);
+  const tabItems = [
+    { name: 'Collateral', content: <Holdings address={address as string} /> },
+    { name: 'Loans', content: <>Coming Soon</> },
+    { name: 'Offers', content: <>Coming Soon</> },
+  ];
 
   return address ? (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Your Collection</h2>
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {holdings?.map((asset: { [key: string]: any }, index: number) => {
-            const imageSrc = asset?.token?.thumbnail_uri?.replace(
-              'ipfs://',
-              'https://ipfs.io/ipfs/'
-            );
-
-            return (
-              <div key={`holding-${index}`} className="group relative">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                  <img
-                    src={imageSrc}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      <Link href={`borrow/${asset?.token?.fa2_address}/${asset?.token?.token_id}`}>
-                        <a>
-                          <span aria-hidden="true" className="absolute inset-0" />
-                          {asset?.token?.name}
-                        </a>
-                      </Link>
-                    </h3>
-                  </div>
-                  <p className="text-sm font-medium text-gray-900">{asset?.token?.platform}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+      <div className="max-w-2xl mx-auto py-12 px-4 sm:py-20 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+          <span className="block xl:inline">Borrow</span>
+          <span className="block text-indigo-600 xl:inline"> against your assets</span>
+        </h1>
+      </div>
+      <div className="max-w-2xl mx-auto px-2 sm:px-6 lg:max-w-7xl lg:px-8">
+        <Tabs tabItems={tabItems} />
       </div>
     </div>
   ) : (
