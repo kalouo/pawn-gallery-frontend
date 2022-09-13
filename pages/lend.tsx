4dafc104db045de14e369747e10fe076d930e8be
kalouo <23177052+kalouo@users.noreply.github.com>
2022-09-13 10:53:00 +0200

    fix: key prop

--- pages/lend.tsx
+++ pages/lend.tsx
@@ -23,9 +23,9 @@ const LiveRequests = ({ address }: { address: string }) => {
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {requests?.data?.map((request, index) => {
           return (
-            <Link href={`/lend/${request.requestId}`}>
+            <Link href={`/lend/${request.requestId}`} key={`holding-${index}`}>
               <a>
-                <div key={`holding-${index}`} className="group relative">
+                <div className="group relative">
                   <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                     <img
                       src={request.imageUrl}
